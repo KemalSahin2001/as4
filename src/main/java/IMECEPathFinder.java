1560f4b77c81e91dccfc5074e131cae3d8457551
Kemal Şahin <kemals@rocketmail.com>
2023-05-31 11:21:30 +0300

    assignment4

--- src/main/java/IMECEPathFinder.java
+++ src/main/java/IMECEPathFinder.java
@@ -4,89 +4,165 @@ import java.awt.*;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class IMECEPathFinder{
-	  public int[][] grid;
-	  public int height, width;
-	  public int maxFlyingHeight;
-	  public double fuelCostPerUnit, climbingCostPerUnit;
+public class IMECEPathFinder {
+	public int[][] grid; // 2D array to store the grid data
+	private int[][] scaledGrid; // 2D array to store the scaled grid data
+	public int height; // Number of rows in the grid
+	public int width; // Number of columns in the grid
+	public int maxFlyingHeight; // Maximum flying height
+	public double fuelCostPerUnit; // Fuel cost per unit distance
+	public double climbingCostPerUnit; // Climbing cost per unit height difference
 
-	public IMECEPathFinder(String filename, int rows, int cols, int maxFlyingHeight, double fuelCostPerUnit, double climbingCostPerUnit) {
+	public IMECEPathFinder(String filename, int rows, int cols, int maxFlyingHeight, double fuelCostPerUnit,
+			double climbingCostPerUnit) {
+		// Initialize the grid and scaledGrid arrays with the specified number of rows
+		// and columns
 		grid = new int[rows][cols];
+		scaledGrid = new int[rows][cols];
+
+		// Set the height and width of the grid
 		this.height = rows;
 		this.width = cols;
+
+		// Set the maximum flying height, fuel cost per unit, and climbing cost per unit
 		this.maxFlyingHeight = maxFlyingHeight;
 		this.fuelCostPerUnit = fuelCostPerUnit;
 		this.climbingCostPerUnit = climbingCostPerUnit;
 
 		try {
+			// Create a Scanner object to read from the file
 			Scanner scanner = new Scanner(new File(filename));
+
+			// Read the grid values from the file and store them in the grid array
 			for (int i = 0; i < rows; i++) {
 				for (int j = 0; j < cols; j++) {
 					grid[i][j] = scanner.nextInt();
 				}
 			}
+
+			// Close the scanner
 			scanner.close();
+
+			// Save a copy of the original grid by calling the saveGrid method
+			saveGrid();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	  /**
-	   * Draws the grid using the given Graphics object.
-	   * Colors should be grayscale values 0-255, scaled based on min/max elevation values in the grid
-	   */
-	  public void drawGrayscaleMap(Graphics g) {
-		  // Find the minimum and maximum elevation values in the grid
-		  int minElevation = Integer.MAX_VALUE;
-		  int maxElevation = Integer.MIN_VALUE;
-		  for (int i = 0; i < grid.length; i++) {
-			  for (int j = 0; j < grid[0].length; j++) {
-				  int elevation = grid[i][j];
-				  if (elevation < minElevation) {
-					  minElevation = elevation;
-				  }
-				  if (elevation > maxElevation) {
-					  maxElevation = elevation;
-				  }
-			  }
-		  }
+	private void saveGrid() {
 
-		  // Draw the grid using grayscale colors based on the elevation values
-		  for (int i = 0; i < grid.length; i++) {
-			  for (int j = 0; j < grid[0].length; j++) {
-				  int elevation = grid[i][j];
-				  int grayscale = mapToGrayscale(elevation, minElevation, maxElevation);
-				  g.setColor(new Color(grayscale, grayscale, grayscale));
-				  g.fillRect(j, i, 1, 1);
-			  }
-		  }
-	  }
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] < min) {
+					min = grid[i][j];
+				}
+				if (grid[i][j] > max) {
+					max = grid[i][j];
+				}
+			}
+		}
+		// find the min and max values in the grid
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				scaledGrid[i][j] = (int) ((grid[i][j] - min) * 255.0 / (max - min));
+			}
+		}
+		// scale the grid values to 0-255 using the min and max values
+		try {
+			PrintWriter writer = new PrintWriter("grayscaleMap.dat");
+			for (int i = 0; i < scaledGrid.length; i++) {
+				for (int j = 0; j < scaledGrid[0].length; j++) {
+					if (j != scaledGrid[0].length - 1)
+						writer.print(scaledGrid[i][j] + " ");
+					else
+						writer.print(scaledGrid[i][j]);
+				}
+				if (i < scaledGrid.length - 1)
+					writer.println();
+			}
+			writer.close();
+			// write the scaled grid to a file called "grayscaleMap.dat"
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Draws the grid using the given Graphics object.
+	 * Colors should be grayscale values 0-255, scaled based on min/max elevation
+	 * values in the grid
+	 */
+	public void drawGrayscaleMap(Graphics g) {
+		// Find the minimum and maximum elevation values in the grid
+		int minElevation = Integer.MAX_VALUE;
+		int maxElevation = Integer.MIN_VALUE;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				int elevation = grid[i][j];
+				if (elevation < minElevation) {
+					minElevation = elevation;
+				}
+				if (elevation > maxElevation) {
+					maxElevation = elevation;
+				}
+			}
+		}
+
+		// Draw the grid using grayscale colors based on the elevation values
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				int elevation = grid[i][j];
+				int grayscale = mapToGrayscale(elevation, minElevation, maxElevation);
+				g.setColor(new Color(grayscale, grayscale, grayscale));
+				g.fillRect(j, i, 1, 1);
+			}
+		}
+	}
 
 	// Helper method to map an elevation value to a grayscale color value
 	private int mapToGrayscale(int value, int minValue, int maxValue) {
+		// Calculate the range of values
 		int range = maxValue - minValue;
+
+		// Scale the value by subtracting the minimum value
 		int scaledValue = value - minValue;
+
+		// Map the scaled value to the grayscale range (0-255)
 		int grayscale = (int) (scaledValue / (double) range * 255);
+
+		// Return the grayscale value
 		return grayscale;
 	}
 
 	private double calculateCost(Point p1, Point p2) {
+		// Calculate the horizontal and vertical distance between the two points
 		int dx = p2.x - p1.x;
 		int dy = p2.y - p1.y;
 		double dist = Math.sqrt(dx * dx + dy * dy);
 
+		// Get the current height and neighbor height from the grid
 		int currentHeight = grid[p1.y][p1.x];
 		int neighborHeight = grid[p2.y][p2.x];
+
+		// Calculate the height impact based on the difference between the current and
+		// neighbor heights
 		double heightImpact = (currentHeight >= neighborHeight) ? 0 : (neighborHeight - currentHeight);
 
+		// Calculate the cost as the distance multiplied by the fuel cost per unit plus
+		// the climbing cost per unit multiplied by the height impact
 		return (dist * fuelCostPerUnit) + (climbingCostPerUnit * heightImpact);
 	}
 
-
-
 	/**
-	 * Get the most cost-efficient path from the source Point start to the destination Point end
+	 * Get the most cost-efficient path from the source Point start to the
+	 * destination Point end
 	 * using Dijkstra's algorithm on pixels.
+	 * 
 	 * @return the List of Points on the most cost-efficient path from start to end
 	 */
 
@@ -97,19 +173,20 @@ public class IMECEPathFinder{
 
 		// create cost grid and parent grid
 		double[][] costGrid = new double[height][width];
-		for (double[] row : costGrid) Arrays.fill(row, Double.MAX_VALUE);
+		for (double[] row : costGrid)
+			Arrays.fill(row, Double.MAX_VALUE);
 
 		Point[][] parentGrid = new Point[height][width];
 
 		// movement directions
-		int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+		int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
 
 		// start point cost
 		costGrid[start.y][start.x] = 0;
 
 		// using a priority queue for Dijkstra's algorithm
-		PriorityQueue<Point> queue = new PriorityQueue<>((a, b) ->
-				Double.compare(costGrid[a.y][a.x], costGrid[b.y][b.x]));
+		PriorityQueue<Point> queue = new PriorityQueue<>(
+				(a, b) -> Double.compare(costGrid[a.y][a.x], costGrid[b.y][b.x]));
 
 		queue.add(start);
 
@@ -117,14 +194,16 @@ public class IMECEPathFinder{
 			Point current = queue.poll();
 
 			// if we reached the end point
-			if (current.equals(end)) break;
+			if (current.equals(end))
+				break;
 
 			for (int[] dir : dirs) {
 				int newX = current.x + dir[0];
 				int newY = current.y + dir[1];
 
 				// check grid boundaries and maxFlyingHeight
-				if (newX < 0 || newY < 0 || newX >= width || newY >= height || grid[newY][newX] > maxFlyingHeight) continue;
+				if (newX < 0 || newY < 0 || newX >= width || newY >= height || grid[newY][newX] > maxFlyingHeight)
+					continue;
 
 				// calculate cost
 				double newCost = calculateCost(current, new Point(newX, newY));
@@ -142,7 +221,7 @@ public class IMECEPathFinder{
 		// construct path from end to start
 		List<Point> path = new ArrayList<>();
 		for (Point p = end; p != null; p = parentGrid[p.y][p.x]) {
-			path.add(0, p);  // add at the start to reverse the path
+			path.add(0, p); // add at the start to reverse the path
 		}
 
 		return path;
@@ -150,7 +229,9 @@ public class IMECEPathFinder{
 
 	/**
 	 * Calculate the most cost-efficient path from source to destination.
-	 * @return the total cost of this most cost-efficient path when traveling from source to destination
+	 * 
+	 * @return the total cost of this most cost-efficient path when traveling from
+	 *         source to destination
 	 */
 
 	public double getMostEfficientPathCost(List<Point> path) {
@@ -169,23 +250,24 @@ public class IMECEPathFinder{
 		return totalCost;
 	}
 
-
-
 	/**
-	 * Draw the most cost-efficient path on top of the grayscale map from source to destination.
+	 * Draw the most cost-efficient path on top of the grayscale map from source to
+	 * destination.
 	 */
-	public void drawMostEfficientPath(Graphics g, List<Point> path){
-		g.setColor(Color.GREEN);  // Set the color of the path to green
+	public void drawMostEfficientPath(Graphics g, List<Point> path) {
+		g.setColor(Color.GREEN); // Set the color of the path to green
 
-		for (Point point : path){
-			g.fillOval(point.x, point.y, 1, 1);  // Draw a small circle (dot) at each point
+		for (Point point : path) {
+			g.fillOval(point.x, point.y, 1, 1); // Draw a small circle (dot) at each point
 		}
 	}
 
-
 	/**
-	 * Find an escape path from source towards East such that it has the lowest elevation change.
-	 * Choose a forward step out of 3 possible forward locations, using greedy method described in the assignment instructions.
+	 * Find an escape path from source towards East such that it has the lowest
+	 * elevation change.
+	 * Choose a forward step out of 3 possible forward locations, using greedy
+	 * method described in the assignment instructions.
+	 * 
 	 * @return the list of Points on the path
 	 */
 	public List<Point> getLowestElevationEscapePath(Point start) {
@@ -196,18 +278,22 @@ public class IMECEPathFinder{
 		pathPointsList.add(start);
 
 		// Traverse towards East
-		while(current.x < grid[0].length - 1) {
+		while (current.x < grid[0].length - 1) {
 			int currentElevation = grid[current.y][current.x];
 
-			// Calculate the absolute difference in elevation for the three possible next points: NE, E, SE
-			int northElevationChange = current.y > 0 ? Math.abs(currentElevation - grid[current.y - 1][current.x + 1]) : Integer.MAX_VALUE;
+			// Calculate the absolute difference in elevation for the three possible next
+			// points: NE, E, SE
+			int northElevationChange = current.y > 0 ? Math.abs(currentElevation - grid[current.y - 1][current.x + 1])
+					: Integer.MAX_VALUE;
 			int eastElevationChange = Math.abs(currentElevation - grid[current.y][current.x + 1]);
-			int southElevationChange = current.y < grid.length - 1 ? Math.abs(currentElevation - grid[current.y + 1][current.x + 1]) : Integer.MAX_VALUE;
+			int southElevationChange = current.y < grid.length - 1
+					? Math.abs(currentElevation - grid[current.y + 1][current.x + 1])
+					: Integer.MAX_VALUE;
 
 			// Choose the path with the least change in elevation
-			if(eastElevationChange <= northElevationChange && eastElevationChange <= southElevationChange) {
+			if (eastElevationChange <= northElevationChange && eastElevationChange <= southElevationChange) {
 				current = new Point(current.x + 1, current.y);
-			} else if(northElevationChange <= southElevationChange) {
+			} else if (northElevationChange <= southElevationChange) {
 				current = new Point(current.x + 1, current.y - 1);
 			} else {
 				current = new Point(current.x + 1, current.y + 1);
@@ -220,42 +306,46 @@ public class IMECEPathFinder{
 		return pathPointsList;
 	}
 
-
-
-
 	/**
-	 * Calculate the escape path from source towards East such that it has the lowest elevation change.
+	 * Calculate the escape path from source towards East such that it has the
+	 * lowest elevation change.
+	 * 
 	 * @return the total change in elevation for the entire path
 	 */
 	public int getLowestElevationEscapePathCost(List<Point> pathPointsList) {
 		int totalChange = 0;
 
+		// Iterate over each pair of adjacent points in the pathPointsList
 		for (int i = 0; i < pathPointsList.size() - 1; i++) {
 			Point currentPoint = pathPointsList.get(i);
 			Point nextPoint = pathPointsList.get(i + 1);
 
-
+			// Get the elevation of the current and next points from the grid
 			int currentElevation = grid[currentPoint.y][currentPoint.x];
 			int nextElevation = grid[nextPoint.y][nextPoint.x];
 
+			// Calculate the absolute difference between the current and next elevations and
+			// add it to the total change
 			totalChange += Math.abs(currentElevation - nextElevation);
 		}
 
+		// Return the total elevation change along the path
 		return totalChange;
 	}
 
-
 	/**
-	 * Draw the escape path from source towards East on top of the grayscale map such that it has the lowest elevation change.
+	 * Draw the escape path from source towards East on top of the grayscale map
+	 * such that it has the lowest elevation change.
 	 */
-	public void drawLowestElevationEscapePath(Graphics g, List<Point> pathPointsList){
-		g.setColor(Color.YELLOW);  // Set the color of the path to yellow
+	public void drawLowestElevationEscapePath(Graphics g, List<Point> pathPointsList) {
+		// Set the color of the path to yellow
+		g.setColor(Color.YELLOW);
 
-		for (Point point : pathPointsList){
-			g.fillOval(point.x, point.y, 1, 1);  // Draw a small circle (dot) at each point
+		// Iterate over each point in the pathPointsList
+		for (Point point : pathPointsList) {
+			// Draw a small circle (dot) at each point
+			g.fillOval(point.x, point.y, 1, 1);
 		}
 	}
-
-
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,59 +4,83 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
         // Step 0 - read the input parameters
-        DatReader datReader = new DatReader("C:\\Users\\xraef\\Desktop\\Java Projects\\assignment44\\src\\main\\java\\sample_IO\\sample_2\\yuksekova_input.dat");
+        DatReader datReader = new DatReader(args[0]);
 
         String grid_input_file_name = datReader.getStringVar("grid_input_file_name");
         int num_rows = datReader.getIntVar("num_rows");
-        int num_cols= datReader.getIntVar("num_cols");
+        int num_cols = datReader.getIntVar("num_cols");
         Point mission_0_source = datReader.getPointVar("mission_0_source");
-        Point mission_0_destination= datReader.getPointVar("mission_0_destination");
+        Point mission_0_destination = datReader.getPointVar("mission_0_destination");
         Point mission_1_source = datReader.getPointVar("mission_1_source");
-        int max_flying_height= datReader.getIntVar("max_flying_height");
-        Double fuel_cost_per_unit= datReader.getDoubleVar("fuel_cost_per_unit");
-        Double climbing_cost_per_unit=datReader.getDoubleVar("climbing_cost_per_unit");
+        int max_flying_height = datReader.getIntVar("max_flying_height");
+        Double fuel_cost_per_unit = datReader.getDoubleVar("fuel_cost_per_unit");
+        Double climbing_cost_per_unit = datReader.getDoubleVar("climbing_cost_per_unit");
 
         // Step 1 - construct map data
-        IMECEPathFinder map = new IMECEPathFinder("C:\\Users\\xraef\\Desktop\\Java Projects\\assignment44\\src\\main\\java\\sample_IO\\sample_2\\" + grid_input_file_name,
+        IMECEPathFinder map = new IMECEPathFinder(grid_input_file_name,
                 num_rows, num_cols, max_flying_height, fuel_cost_per_unit, climbing_cost_per_unit);
-
 
         // Step 2 - construct DrawingPanel, and get its Graphics context
         // COMMENT OUT THESE LINES BEFORE TURBO TESTING AND SUBMISSION
-        DrawingPanel panel = new DrawingPanel(num_rows, num_cols);
-        Graphics g = panel.getGraphics();
-        map.drawGrayscaleMap(g);
 
+        // Create a new DrawingPanel object with the specified number of rows and
+        // columns
+        // DrawingPanel panel = new DrawingPanel(num_rows, num_cols);
 
-        // Step 3 - get the most cost-efficient path between source and destination Points
+        // Get the Graphics object from the panel
+        // Graphics g = panel.getGraphics();
+
+        // Call the drawGrayscaleMap method of the map object and pass the Graphics
+        // object as a parameter
+        // map.drawGrayscaleMap(g);
+
+        // Step 3 - get the most cost-efficient path between source and destination
+        // Points
         System.out.println("########## Mission 0 ##########");
+
+        // Calculate the shortest path using the getMostEfficientPath method of the map
+        // object
         List<Point> shortestPath = map.getMostEfficientPath(mission_0_source, mission_0_destination);
-        if (shortestPath.size() == 0){
+
+        // Check if the shortest path is empty
+        if (shortestPath.size() == 0) {
             System.out.println("ERROR PathNotFound: There is no most cost-efficient path that meets all criteria!");
-        }
-        else{
+        } else {
             System.out.println("The most cost-efficient path's size: " + shortestPath.size());
+
+            // Calculate the total cost of the shortest path using the
+            // getMostEfficientPathCost method of the map object
             double totalCost = map.getMostEfficientPathCost(shortestPath);
             System.out.println("The most cost-efficient path has a cost of: " + totalCost);
 
-
-            //map.drawMostEfficientPath(g, shortestPath); // COMMENT OUT THIS LINE BEFORE TURBO TESTING AND SUBMISSION
-
+            // map.drawMostEfficientPath(g, shortestPath); // COMMENT OUT THIS LINE BEFORE
+            // TURBO TESTING AND SUBMISSION
         }
-        // Step 4 - get the lowest elevation Escape Path towards the West from a source Point
+
+        // Step 4 - get the lowest elevation Escape Path towards the West from a source
+        // Point
         System.out.println("########## Mission 1 ##########");
+
+        // Calculate the escape path with the least elevation cost using the
+        // getLowestElevationEscapePath method of the map object
         List<Point> leastElevationPath = map.getLowestElevationEscapePath(mission_1_source);
+
+        // Print the size of the escape path with the least elevation cost
         System.out.println("The size of the escape path with the least elevation cost: " + leastElevationPath.size());
+
+        // Calculate the total elevation change of the escape path using the
+        // getLowestElevationEscapePathCost method of the map object
         int totalChange = map.getLowestElevationEscapePathCost(leastElevationPath);
+
+        // Print the least elevation cost of the escape path
         System.out.println("The escape path has the least elevation cost of: " + totalChange);
 
-
-        map.drawLowestElevationEscapePath(g, leastElevationPath);  // COMMENT OUT THIS LINE BEFORE TURBO TESTING AND SUBMISSION
+        // map.drawLowestElevationEscapePath(g, leastElevationPath); // COMMENT OUT THIS
+        // LINE BEFORE TURBO TESTING AND SUBMISSION
 
     }
 }
